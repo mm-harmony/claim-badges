7925ca1efede529a1e0da4aa93f7ef0b828a9fd0
Mustapha <141042873+mm-harmony@users.noreply.github.com>
2025-03-01 11:47:58 +0000

    Update console.js: add new function

--- console.js
+++ console.js
@@ -1,3 +1,10 @@
 // print "hello, world!" with console.log
-console.log('hello, world!')
-console.log('-------------') // add style
+console.log('hello, world!');
+console.log('-------------'); // add style
+
+// print your name
+function printName(name){
+  console.log(name)
+}
+
+printName("heymmusttech");
