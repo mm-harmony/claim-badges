ff77075b32b49ed6c0fe74ac69d31224fbab075d
Mustapha <141042873+mm-harmony@users.noreply.github.com>
2025-03-01 11:49:09 +0000

    Remove changes

--- console.js
+++ console.js
@@ -1,10 +1,3 @@
 // print "hello, world!" with console.log
-console.log('hello, world!');
-console.log('-------------'); // add style
-
-// print your name
-function printName(name){
-  console.log(name)
-}
-
-printName("heymmusttech");
+console.log('hello, world!')
+console.log('-------------') // add style
